c36d9bdf3634fb63f1fcfab71bfc5eb4612d45da
刘玉文 <294032172@qq.com>
2019-07-29 19:24:21 +0800

    商品详情

--- ios/supermarketapp/AppDelegate.h
+++ ios/supermarketapp/AppDelegate.h
@@ -10,6 +10,6 @@
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
-@property (nonatomic, strong) UIWindow *window;
+//@property (nonatomic, strong) UIWinKdow *window;
 
 @end
