91e21ff38a5752afe2dd95fc19b0b556780dc868
李华良 <80750784@yonghui.cn>
2019-07-30 22:04:26 +0800

    update app register

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 /**
  * @format
  */
-
 import {AppRegistry} from 'react-native'
-//import App from './src/App'
-//import {name as appName} from './app.json'
-//import CMS from './src/pages/CMS'
+import CMS from './src/pages/CMS'
 import Root from './src/Root'
+
 AppRegistry.registerComponent('RNProductDetail', () => Root)
-// AppRegistry.registerComponent(appName, () => App)
-// AppRegistry.registerComponent('RNCMS', () => CMS)
+AppRegistry.registerComponent('RNCMS', () => CMS)
