4a3b4885da46723a36350339ef9be20b8f10efc3
李华良 <80750784@yonghui.cn>
2019-08-28 18:45:12 +0800

    fix android tab bar

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ import HotSaleBoard from './src/pages/HotSaleBoard'
 import Found from './src/pages/Found'
 import Activity from './src/pages/Activity'
 
+// 隐藏 RN yellowBox warning 提示
+console.disableYellowBox = true
+
 AppRegistry.registerComponent('RNProductDetail', () => Root)
 AppRegistry.registerComponent('RNHome', () => Home)
 AppRegistry.registerComponent('RNFound', () => Found)
