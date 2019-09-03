9215ec47ed32719a149ac1088b546ff0fec09d98
李华良 <80750784@yonghui.cn>
2019-09-03 14:56:09 +0800

    update theme color file

---
+++ src/theme/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: 李华良 
+ * @Date: 2019-09-03 14:51:59 
+ * @Last Modified by: 李华良
+ * @Last Modified time: 2019-09-03 14:53:02
+ */
+import * as theme from './light-green'
+
+export default theme
---
+++ src/theme/light-green.ts
@@ -0,0 +1,26 @@
+/**
+ * 主题色
+ * 颜色 0 ～ 10 逐渐变浅
+ * @Author: 李华良 
+ * @Date: 2019-09-03 14:50:25 
+ * @Last Modified by: 李华良
+ * @Last Modified time: 2019-09-03 14:55:10
+ */
+export const primary = '#82BF3C'
+export const secondary = '#97CC5B'
+
+export const white = '#FFFFFF'
+
+export const black = '#333333'
+export const black1 = '#666666'
+
+export const gray = '#A3A3A3'
+export const gray1 = '#B3B3B3'
+export const gray10 = '#F6F6F6'
+
+export const orange = '#FA8500'
+export const orange10 = '#FFE5E0'
+
+export const red = '#FF3914'
+export const red1 = '#FF6042'
+export const red10 = '#FFDED9'
