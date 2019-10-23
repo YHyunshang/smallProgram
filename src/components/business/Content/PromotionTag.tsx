c17af5da31a0a25a34799073164761fa365eec4a
李华良 <80750784@yonghui.cn>
2019-10-23 17:45:52 +0800

    fix iphone xs max 满减标签

--- src/components/business/Content/PromotionTag.tsx
+++ src/components/business/Content/PromotionTag.tsx
@@ -4,34 +4,24 @@ import { View, Text, StyleSheet } from 'react-native'
 interface Props {
   title: string
   content: string
-  bgColor?: string
-  customStyles?: {
-    [index: string]: {}
-  }
+  primaryColor?: string
+  secondaryColor?: string
 }
 
 export default function PromotionTag({
   title,
   content,
-  bgColor = '#F24458',
-  customStyles = {},
+  primaryColor = '#F24458',
+  secondaryColor = '#FFF'
 }: Props) {
   return (
-    <View
-      style={[
-        styles.container,
-        { borderColor: bgColor },
-        customStyles.container,
-      ]}
-    >
-      <Text
-        style={[styles.title, { backgroundColor: bgColor }, customStyles.title]}
-      >
-        {title}
-      </Text>
-      <Text style={[styles.content, { color: bgColor }, customStyles.content]}>
-        {content}
-      </Text>
+    <View style={styles.container}>
+      <View style={[ styles.titleContainer, { backgroundColor: primaryColor } ]}>
+        <Text style={[ styles.title, { color: secondaryColor } ]}>{title}</Text>
+      </View>
+      <View style={[ styles.contentContainer, { borderColor: primaryColor } ]}>
+        <Text style={[ styles.content, { color: primaryColor } ]}>{content}</Text>
+      </View>
     </View>
   )
 }
@@ -40,21 +30,31 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     alignItems: 'center',
-    height: 17,
-    borderWidth: 1,
-    borderRadius: 1,
     overflow: 'hidden',
     marginRight: 5,
   },
-  title: {
-    lineHeight: 15,
-    color: '#FFF',
-    fontSize: 11,
+
+  titleContainer: {
+    borderTopLeftRadius: 1,
+    borderBottomLeftRadius: 1,
     paddingHorizontal: 5,
+    height: 17,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 11,
+  },
+
+  contentContainer: {
+    borderTopRightRadius: 1,
+    borderBottomRightRadius: 1,
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    paddingHorizontal: 7,
+    height: 17,
+    justifyContent: 'center',
   },
   content: {
     fontSize: 11,
-    lineHeight: 15,
-    paddingHorizontal: 7,
   },
 })
