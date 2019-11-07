eb466bc6933c44e86cb1d27912632d42ff11f292
李华良 <80750784@yonghui.cn>
2019-11-07 18:33:38 +0800

    fix 商详页滚动

--- src/pages/ProductDetail/ProductDetailPage.js
+++ src/pages/ProductDetail/ProductDetailPage.js
@@ -373,9 +373,6 @@ export default class ProductDetailPage extends React.Component {
                 {shopImgList}
               </View>
             </View>
-
-            {/*底部 native 购物车栏占位*/}
-            <View style={{ height: 50 }} />
           </ScrollView>
         </View>
 
