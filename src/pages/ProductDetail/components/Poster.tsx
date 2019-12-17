bd04bfd356acc7ebc1464868e2a8a5102bf8a623
李华良 <80750784@yonghui.cn>
2019-12-17 09:38:28 +0800

    update 商详页保存图片后提示

--- src/pages/ProductDetail/components/Poster.tsx
+++ src/pages/ProductDetail/components/Poster.tsx
@@ -2,12 +2,13 @@
  * Created by 李华良 on 2019-11-27
  */
 import * as React from 'react'
-import {PopUp} from "@components";
-import {View, Text} from "react-native";
+import { PopUp } from '@components'
+import { View, Text } from 'react-native'
 import styles from './Poster.styles'
-import FastImage from "react-native-fast-image";
-import {TouchableOpacity} from "react-native-gesture-handler";
-import {download} from "@utils/img";
+import FastImage from 'react-native-fast-image'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { download } from '@utils/img'
+import { showToast } from '@utils/native'
 
 export interface PosterProps {
   visible: boolean
@@ -15,8 +16,17 @@ export interface PosterProps {
   image: string
 }
 
-const Poster: React.FunctionComponent<PosterProps> = ({ visible, image, onClose }) => {
-  const saveImg = () => download(image)
+const Poster: React.FunctionComponent<PosterProps> = ({
+  visible,
+  image,
+  onClose,
+}) => {
+  const saveImg = () =>
+    download(image).then(() => {
+      showToast('图片保存成功', '1')
+      onClose instanceof Function && setTimeout(onClose, 500)
+    })
+
   return (
     <PopUp visible={visible} title="保存至相册" onClose={onClose}>
       <View style={styles.container}>
@@ -26,7 +36,9 @@ const Poster: React.FunctionComponent<PosterProps> = ({ visible, image, onClose 
             <Text style={styles.btnText}>保存图片</Text>
           </View>
         </TouchableOpacity>
-        <Text style={styles.tips}>保存到手机相册后，将图片分享到您的朋友圈</Text>
+        <Text style={styles.tips}>
+          保存到手机相册后，将图片分享到您的朋友圈
+        </Text>
       </View>
     </PopUp>
   )
