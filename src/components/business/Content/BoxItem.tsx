62c273ab17b1072fa97095034c4b35eb75a4bea0
李华良 <80750784@yonghui.cn>
2020-01-13 14:59:01 +0800

    update 分类入口增加占位图

--- src/components/business/Content/BoxItem.tsx
+++ src/components/business/Content/BoxItem.tsx
@@ -3,15 +3,12 @@
  * Created by 李华良 on 2019-07-04
  */
 import * as React from 'react'
-import {
-  View,
-  Text,
-  TouchableWithoutFeedback,
-} from 'react-native'
+import { View, Text, TouchableWithoutFeedback, Animated } from 'react-native'
 import { Native, Img } from '@utils'
 import styles from './BoxItem.styles'
 import { placeholderBox } from '@const/resources'
 import FastImage from 'react-native-fast-image'
+import { usePlaceholder } from '@utils/hooks'
 
 export interface Props {
   link: Native.Navigation // 跳转链接
@@ -21,11 +18,28 @@ export interface Props {
 
 export default function BoxItem({ link, image, title }: Props) {
   const fitImg = image ? { uri: Img.loadRatioImage(image, 52) } : placeholderBox
+  const [placeholderVis, placeholderOpacityStyle, onLoad] = usePlaceholder()
 
   return (
     <TouchableWithoutFeedback onPress={() => Native.navigateTo(link)}>
       <View style={styles.box}>
-        <FastImage style={styles.boxImg} source={fitImg} resizeMode={FastImage.resizeMode.contain} />
+        <FastImage
+          style={styles.boxImg}
+          source={fitImg}
+          resizeMode="contain"
+          onLoad={onLoad}
+        />
+        {placeholderVis && (
+          <Animated.View
+            style={[styles.placeholderBox, placeholderOpacityStyle]}
+          >
+            <FastImage
+              style={[styles.placeholder]}
+              source={placeholderBox}
+              resizeMode="contain"
+            />
+          </Animated.View>
+        )}
         <Text
           style={styles.boxText}
           selectable={false}
