353702f6e97ea426cf0eb0ea45e44ef64a46c806
李华良 <80750784@yonghui.cn>
2020-01-15 16:53:06 +0800

    update 半包商品横划占位图

--- src/components/business/Content/ProductSwiperWithBg/ProductSwiperWithBg.styles.ts
+++ src/components/business/Content/ProductSwiperWithBg/ProductSwiperWithBg.styles.ts
@@ -1,20 +1,42 @@
 import { StyleSheet } from 'react-native'
-import {Global} from "@utils";
+import { WindowWidth } from '@utils/global'
 
 export default StyleSheet.create({
   container: {
     position: 'relative',
   },
-  bgImg: {
-    width: '100%',
-    height: Global.WindowWidth * 264 / 375,
+
+  bgBox: {
+    position: 'relative',
     paddingBottom: 56,
   },
+  bgImg: {
+    width: WindowWidth,
+    height: (WindowWidth * 264) / 375,
+  },
+
+  thumbnailPlaceholderBox: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 56,
+    left: 0,
+    zIndex: 1,
+    backgroundColor: '#FBFBFB',
+    alignItems: 'center',
+  },
+  thumbnailPlaceholder: {
+    marginTop: 60,
+    width: 145,
+    height: 40,
+  },
+
   swiper: {
     position: 'absolute',
     bottom: 0,
     left: 7,
     right: 7,
+    zIndex: 2,
     borderRadius: 5,
     overflow: 'hidden',
     backgroundColor: '#fff',
